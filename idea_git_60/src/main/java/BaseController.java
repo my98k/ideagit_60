73e91c4a21bd927948b7f9cf65aadf9c2812325b
sublun <sublun@126.com>
2018-10-02 18:49:45 +0800

    提交的失误

--- idea_git_60/src/main/java/BaseController.java
+++ idea_git_60/src/main/java/BaseController.java
@@ -1,6 +1,6 @@
 public class BaseController {
     public static void main(String[] args) {
         System.out.println("测试提交");
-        System.out.println("提交的失误");
+        System.out.println("左侧的idea");
     }
 }
